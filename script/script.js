ba4b56a752a97825d9cf03a6e2251e91708c0d9c
Tomii <tmedina2003@gmail.com>
2022-09-06 01:39:53 -0300

    JS

--- script/script.js
+++ script/script.js
@@ -17,6 +17,7 @@ function registrar() {
     if (password === rPassword) {
       if (captcha === "valido ingreso") {
         registros.push({username: username, password: password, email: email});
+        console.log("Registrado");
       }
     }
   }  
