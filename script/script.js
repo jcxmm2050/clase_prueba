d3ff96440bab36169f5975b70de94c1c57e33efd
Tomii <tmedina2003@gmail.com>
2022-09-06 07:03:22 -0300

    JS funcionando pero hay que arreglar los objetos (error en ellos)

--- script/script.js
+++ script/script.js
@@ -1,39 +1,54 @@
-var registros = [
-  {
-    username: "test",
-    password: "test01",
-    email: "testing@gmail.com"
+/* Creando la clase */
+
+class Usuario {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
   }
-];
+  get username() {
+    return this.username;
+  }
+  get email() {
+    return this.email;
+  }
+  get password() {
+    return this.password;
+  }
+}
+
+let registros = [];
+
+/* Obteniendo valores del html */
 
 const username = document.getElementById("username");
 const password = document.getElementById("password");
-const rPassword = document.getElementById("r-password");
-const email = document.getElementById("email");
-const captcha = document.getElementById("captcha");
+const rPassword = document.getElementById("rpassword");
+const mail = document.getElementById("email");
+const verif = document.getElementById("verif");
 const botonRegistro = document.getElementById("done");
+const captcha = "valido ingreso";
+/* Funcion al boton */
 
-botonRegistro.addEventListener('click', test);
+botonRegistro.addEventListener('click', registrar);
 
-function test() {
-  console.log("Hola")
-}
+/* Funciones */
 
 function registrar() {
-  if (!existe(email.innerHTML, username.innerHTML)) {
-    if (password.innerHTML === rPassword.innerHTML) {
-      if (captcha.innerHTML === "valido ingreso") {
-        registros.push({username: username, password: password, email: email});
+  if (!existe(mail.innerHTML, username.innerHTML)) {
+    if (password.innerHTML == rPassword.innerHTML) {
+      if (verif.value === captcha) {
+        registros.push(new Usuario(username.innerHTML, email.innerHTML, password.innerHTML));
         console.log("Registrado");
-      }
-    }
-  }  
+      } else console.log('Captcha incorrecto');
+    } else console.log('Contraseña mal colocada');
+  } else console.log('Usuario existente');
 }
 
-function existe(email, username) {
+function existe(mail, usuario) {
   let retorno = false; let i = 0;
   while (i < registros.length && retorno === false) {
-    if (registros[i].username === username || registros[i].email === email) {
+    if (registros[i].username === usuario || registros[i].email === mail) {
       retorno = true;
     }
   }
