5eee0dd6a285d41c815d7840eed8bc4511eeef20
Tomii <tmedina2003@gmail.com>
2022-09-14 23:17:07 -0300

    Login funcionando (Creo que ya esta todo)

--- script/script.js
+++ script/script.js
@@ -13,18 +13,20 @@ const password = document.getElementById("password");
 const rPassword = document.getElementById("rpassword");
 const mail = document.getElementById("email");
 const verif = document.getElementById("verif");
-const botonRegistro = document.getElementById("done");
 const captcha = "valido ingreso";
+const loginMail = document.getElementById("l-mail");
+const loginPass = document.getElementById("l-pass");
 
 /* Funciones */
 
 function registrar() {
   let resultado;
-  if (!existe(mail.innerHTML, usuario.innerHTML)) {
-    if (password.innerHTML == rPassword.innerHTML) {
+  if (!existe(mail.value, usuario.value)) {
+    if (password.value == rPassword.value) {
       if (verif.value === captcha) {
-        let newUser = {nombre: usuario.innerHTML, mail: email.value, pass: password.innerHTML};
+        let newUser = {nombre: usuario.value, mail: email.value, pass: password.value};
         registros.push(newUser);
+        console.log(registros);
         alert("Registrado");
       } else alert('Captcha incorrecto');
     } else alert('Contraseña mal colocada');
@@ -39,6 +41,17 @@ function existe(mail, usuario) {
     } else i++;
   }
   return retorno;
+}
+
+function login() {
+  let i = 0, found = false;
+  while (i < registros.length && found === false) {
+    if (registros[i].mail === loginMail.value && registros[i].pass === loginPass.value) {
+      alert("Te has logeado exitosamente");
+      found = true;
+    } else i++;
+  }
+  if (!found) alert("No se ha encontrado el usuario");
 }
 
 $('#myModal').on('shown.bs.modal', function () {
